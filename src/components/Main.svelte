9f9134d72275a331887c15ea5eaa852cad05f4d9
alex.ferguson <alex.ferguson@incognito.com>
2024-02-11 18:00:16 -0500

    remove second about

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -70,16 +70,6 @@
         My <span class="text-violet-400">favourite tech</span> includes JavaScript,
         Python, Java, Node.js + Express.js & MongoDB!
       </p>
-      <button
-        class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
-      >
-        <div
-          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
-        />
-        <h4 class="relative z-9">
-          <a href="components/Footer.svelte#contact">Get in touch &rarr;</a>
-        </h4>
-      </button>
     </div>
   </section>
 
